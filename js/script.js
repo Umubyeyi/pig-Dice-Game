05dea3783ebc31f57a294e351d6955b8786ff489
umubyeyi dinah <dinahu2020@gmail.com>
2019-01-14 09:33:34 +0200

    I add branch in my pig-Dice-Game

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@
     $("#help").click(function(){
       swal({
         title: "How To Play!",
-        text: "Choose a player to go first. That player throws a die and scores as many points as the total shown on the die providing the die doesn’t roll a 1. The player may continue rolling and accumulating points (but risk rolling a 1) or end his turn. If the player rolls a 1 his turn is over, he loses all points he accumulated that turn, and he passes the die to the next player. Play passes from player to player until a winner is determined. The first player to accumulate 100 or more points wins the game.",
+        text: "Choose a player to go first. That player throws a die and scores as many points as the total shown on the die providing the die doesn’t roll a 1. The player may continue rolling and accumulating points (but risk rolling a 1) or end his turn. If the player rolls a 1 his turn is over, he loses all points he accumulated that turn, and he passes the die to the next player. Play passes from player to player until a winner is determined. The first player to accumulate 50 or more points wins the game.",
         button: "Got it!",
       });
     })
